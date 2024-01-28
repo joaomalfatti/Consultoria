65bab8cae27a250eec210546139da98008d139ae
joaomalfatti <joao.cursos1210@gmail.com>
2024-01-27 20:16:40 -0400

    Foi adicionado GestureDetector nas imagens do nosso menu, falta agora alinhar e terminar a interface

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -8,10 +8,27 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  void _abrirEmpresa(){
+
+  }
+
+  void _abrirServico(){
+
+  }
+
+  void _abrirCliente(){
+
+  }
+
+  void _abrirContato(){
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: const Text("ATM Consultoria",
         style: TextStyle(
@@ -22,7 +39,41 @@ class _HomePageState extends State<HomePage> {
       ) ,
       body: Container(
         padding: EdgeInsets.all(16),
-
+        child: Column(
+          children: <Widget>[
+            Image.asset("imagens/logo.png"),
+            Padding(
+                padding: EdgeInsets.only(top: 32),
+                child: Row(
+                  children: <Widget>[
+                    GestureDetector(
+                      onTap: _abrirEmpresa,
+                      child: Image.asset("imagens/menu_empresa.png"),
+                    ),
+                    GestureDetector(
+                      onTap: _abrirServico,
+                      child: Image.asset("imagens/menu_servico.png"),
+                    ),
+                  ],
+                ),
+            ),
+            Padding(
+                padding: EdgeInsets.only(top: 32),
+                child: Row(
+                  children: <Widget>[
+                    GestureDetector(
+                      onTap: _abrirCliente,
+                      child: Image.asset("imagens/menu_cliente.png"),
+                    ),
+                    GestureDetector(
+                      onTap: _abrirContato,
+                      child: Image.asset("imagens/menu_contato.png"),
+                    ),
+                  ],
+                ),
+            )
+          ],
+        ),
       ),
     );
   }
