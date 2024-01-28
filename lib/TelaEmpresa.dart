0e70620ad2b4aa2baccbd6472c1e418bc023d417
joaomalfatti <joao.cursos1210@gmail.com>
2024-01-27 21:10:56 -0400

    Foi configurado a parte da empresa

--- lib/TelaEmpresa.dart
+++ lib/TelaEmpresa.dart
@@ -10,6 +10,46 @@ class TelaEmpresa extends StatefulWidget {
 class _TelaEmpresaState extends State<TelaEmpresa> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title:
+        const Text("Sobre a Empresa",
+          style: TextStyle(
+            color: Colors.white
+          ),
+        ),
+        backgroundColor: Colors.green,
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Image.asset("imagens/detalhe_empresa.png"),
+                  const Padding(
+                    padding: EdgeInsets.only(left: 10),
+                    child: Text(
+                      "Sobre a empresa",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.deepOrange
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const Padding(
+                  padding: EdgeInsets.only(top: 16),
+                  child: Text(
+                    "Teste"
+                  ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
